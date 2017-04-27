730af69767da431d3f9f35aec04173c30daf535e
jpetrillo <jpetrillo@massart.edu>
2017-04-27 09:30:22 -0400

    deploy

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio
+coming soon page for portfolio site
+
+A [Puppy](https://github.com/Upstatement/generator-puppy) powered project.
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// Mixin Puppy >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Mixin Puppy
+
+
+gulp.task('default', [ 'puppy-serve' ]);
